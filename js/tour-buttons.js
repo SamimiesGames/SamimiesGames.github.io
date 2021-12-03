8db5157341b43e9b7e7e5c3f4aa1277afb2936eb
SamimiesGames <sami.laatikainen@saunalahti.fi>
2021-12-03 13:01:20 +0200

    `fix(*)` fix everything.

--- js/tour-buttons.js
+++ js/tour-buttons.js
@@ -8,20 +8,6 @@ export function applyTourButtonsExtension() {
 function on_click_tour_button(e) {
   e = e.target
 
-  if(e.dataset.clicked === "1") {
-    e.parentElement.removeChild(e.parentElement.getElementsByTagName("iframe")[0])
-    e.dataset.clicked = "0"
-    e.textContent = "Take a peek."
-  }
-  else {
-    e.dataset.clicked = "1"
+  window.open(e.dataset.src, "_blank")
 
-    const iframe = document.createElement("iframe")
-    iframe.frameBorder = "0"
-    iframe.src = e.dataset.src
-    iframe.width = "567"
-    iframe.height = "720"
-    e.parentElement.appendChild(iframe)
-    e.textContent = "Back"
-  }
 }
